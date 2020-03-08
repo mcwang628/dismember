29e792a6e29ac4415f6b69a76533e1715a9b73a9
Apple <lorettali@gatech.edu>
2020-03-08 18:45:25 -0400

    alpha done done

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] doors;
+    public GameObject[] enemies;
+    public int enemiesDefeated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemiesDefeated == 1)
+        {
+            doors[0].GetComponent<Animator>().SetBool("Open", true);
+            enemies[1].SetActive(true);
+        }
+        if (enemiesDefeated == 2)
+        {
+            doors[1].GetComponent<Animator>().SetBool("Open", true);
+            enemies[2].SetActive(true);
+        }
+        if (enemiesDefeated == 3)
+        {
+            doors[2].GetComponent<Animator>().SetBool("Open", true);
+            enemies[3].SetActive(true);
+        }
+        if (enemiesDefeated == 4)
+        {
+            SceneManager.LoadScene("StartScene");
+        }
+    }
+}
